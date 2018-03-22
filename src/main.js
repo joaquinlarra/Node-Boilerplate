d0f4161e11922ae9c1a43413292f3cb5fd8a31ba
Joaquin Astelarra <joaquinastelarra@gmail.com>
2018-03-22 14:08:55 -0300

    update: log stabilized

--- src/main.js
+++ src/main.js
@@ -3,6 +3,5 @@ const { serverConfig } = require('./config');
 const { utils } = require('./utils');
 
 const log = utils.logger;
-log.info('teste');
-server.listen(serverConfig.port, () => console.log(`Server [ON] - Port: ${serverConfig.port}`));
-// server.listen(serverConfig.port, () => log.info(`Server [ON] - Port: ${serverConfig.port}`));
+
+server.listen(serverConfig.port, () => log.info(`Server [ON] - Port: ${serverConfig.port}`));
--- src/utils/logger.js
+++ src/utils/logger.js
@@ -7,21 +7,26 @@ class Logger {
     }
 
     create() {
+        let rootFolder = path.dirname(__dirname).split(path.sep).pop();
+        rootFolder = path.join(rootFolder + "/log/general.log");
         return new bunyan.createLogger({
             name: this.name,
-            //     streams: [
-            //         {
-            //             type: 'rotating-file',
-            //             path: path.normalize(__dirname + '../log'),
-            //             period: '1d',   // daily rotation
-            //             count: 3        // keep 3 back copies
-            //         },
-            //         {
-            //             stream: process.stderr,
-            //             level: 'debug'
-            //         },
-
-            //     ]
+                streams: [
+                    {
+                        type: 'rotating-file',
+                        path: rootFolder,
+                        period: '1d',   // daily rotation
+                        count: 3        // keep 3 back copies
+                    },
+                    {
+                        level: 'info',
+                        stream: process.stderr,
+                    },
+                    {
+                        stream: process.stderr,
+                        level: 'debug'
+                    },
+                ]
         });
     }
 }
